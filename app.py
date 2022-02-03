a78141aca8b2fb63073f9de2ea08a95b848cb172
Chukwuebuka Valentine Okoye-Nobert <o.chukwuebu@alustudent.com>
2022-02-03 20:50:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,18 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 label_enc = LabelEncoder()
 
-
-
-# clean_doc=pickle.load(open('clean_doc.obj','rb'))
 tfid= pickle.load(open('Tfidfmodels.pkl','rb'))
-model=pickle.load(open('kmeanmodel.pkl','rb'))      
+model=pickle.load(open('kmeanmodel.pkl','rb'))    
+
+
 # front end elements of the web page 
 html_temp = """ 
 <div style ="background-color:blue;padding:5px;"> 
 <h4 style ="color:black;text-align:center;">Streamlit New Article Clustering App</h4> 
 </div> 
 """ 
+
+
 st.markdown(html_temp, unsafe_allow_html = True) 
 default_value_goes_here = ""
 Content = st.text_area("Please enter news from any source below: ", default_value_goes_here)
